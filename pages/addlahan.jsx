3935e89cd07dcb103b8762ec2bf71c2bf20b9e47
syaanwar26 <sya.anwar26@gmail.com>
2022-09-29 09:07:21 +0700

    Tambah lahan

---
+++ pages/addlahan.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import ComEditLahan from '../component/comeditlahan';
+import Footer from '../component/footer';
+import NavbarComp from '../component/navbarcomp';
+import Sidebar from '../component/sidebar';
+
+const addlahan = () => {
+  return (
+    <div>
+        <NavbarComp />
+        <Row className="gx-0 min-vh-100">
+            <Col lg={4}>
+                <div className='sdbr'>
+                    <Sidebar />
+                </div>
+            </Col>
+            <Col lg={8}>
+                <ComEditLahan />
+            </Col>
+        </Row>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default addlahan
