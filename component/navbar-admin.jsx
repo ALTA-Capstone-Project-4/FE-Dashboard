faa6d54dad2bd6ec88742885b7517a71e27e8554
unknown <syifaulya0610@gmail.com>
2022-10-03 23:32:09 +0700

    pisahin navbar

---
+++ component/navbar-admin.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+
+const NavbarAdmin = () => {
+  return (
+    <div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="shadow-sm p-3  bg-body rounded fw-semibold fixed-top"
+      >
+        <Container className="px-0">
+          <Navbar.Brand href="#home">
+            <p className="yellow-font">
+              <span className="green-font">WARE</span>HOUSE
+            </p>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav
+            style={{ maxHeight: '100px' }}
+            navbarScroll>
+              <Nav.Link href="#home"><p className="yellow-font-hover">Beranda</p></Nav.Link>
+              <Nav.Link href="#tentang-kami"><p className="yellow-font-hover">Tentang Kami</p></Nav.Link>
+              <Nav.Link><FontAwesomeIcon icon={faUserCircle} size="xl" className="yellow-font"/></Nav.Link>
+                <NavDropdown  title={<span className="yellow-font-hover" alignRight>Hello, Admin</span>}>
+                  <NavDropdown.Item href="#action/3.1" className="yellow-font-hover fw-semibold">Verifikasi</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4" className="yellow-font-hover fw-semibold">Keluar</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavbarAdmin
--- component/navbarcomp.jsx
+++ component/navbarcomp.jsx
@@ -1,33 +1,44 @@
 import React from 'react'
 import { Nav, Navbar, Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 function NavbarComp() {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className="shadow-sm p-3  bg-body rounded fw-semibold fixed-top">
-      <Container className="px-0">
-        <Navbar.Brand href="#home"><p className="yellow-font"><span className='green-font'>WARE</span>HOUSE</p></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
-          <Nav>
-            <Nav.Link href="/home"><p className="yellow-font-hover">Beranda</p></Nav.Link>
-            <Nav.Link href="/"><p className="yellow-font-hover">Tentang Kami</p></Nav.Link>
-            <Nav.Link href="/registermitra"><p className="yellow-font-hover">Menjadi Mitra</p></Nav.Link>
-            <Nav.Link href="/registerclient"><p className="yellow-font-hover">Menjadi Client</p></Nav.Link>
-            <Nav.Link href="/login"><p className="yellow-font-hover">Masuk</p ></Nav.Link>
-          </Nav>
-          <Nav>
-          <Nav><p className="yellow-font">Hello, User</p ><FontAwesomeIcon icon={faUserCircle} size='xl' className='yellow-font-hover'/></Nav>
-          <Nav.Link></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="shadow-sm p-3  bg-body rounded fw-semibold fixed-top"
+      >
+        <Container className="px-0">
+          <Navbar.Brand href="#home">
+            <p className="yellow-font">
+              <span className="green-font">WARE</span>HOUSE
+            </p>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <Nav.Link href="#home">
+                <p className="yellow-font-hover">Beranda</p>
+              </Nav.Link>
+              <Nav.Link href="#tentang-kami">
+                <p className="yellow-font-hover">Tentang Kami</p>
+              </Nav.Link>
+              <Nav.Link href="#menjadi-mitra">
+                <p className="yellow-font-hover">Menjadi Mitra</p>
+              </Nav.Link>
+              <Nav.Link href="#menjadi-client">
+                <p className="yellow-font-hover">Menjadi Client</p>
+              </Nav.Link>
+              <Nav.Link href="#login">
+                <p className="yellow-font-hover">Masuk</p>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
