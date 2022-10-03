b42b483bd710f33c6d4d73a82e9c1368001d7b52
unknown <syifaulya0610@gmail.com>
2022-10-03 22:32:42 +0700

    add delete and put axios(verification page done)

--- pages/adminverification.jsx
+++ pages/adminverification.jsx
@@ -46,6 +46,54 @@ const adminverification = () => {
       });
   };
 
+  const accept = (id) => {
+    axios
+    .put(`https://group4.altaproject.online/mitra/verify/${id}`,
+    {
+      status: "verified"
+    },
+    {
+      headers : { Authorization: `Bearer ${getCookie("Token")}`}
+    }
+    )
+
+    .then((res) => {
+      alert("berhasil diverifikasi")
+      getVerifMitra();
+    })
+  }
+
+  const denied = (id) => {
+    axios
+    .put(`https://group4.altaproject.online/mitra/verify/${id}`,
+    {
+      status: "rejected"
+    },
+    {
+      headers : { Authorization: `Bearer ${getCookie("Token")}`}
+    }
+    )
+
+    .then((res) => {
+      alert("berhasil dihapus dari daftar verifikasi")
+      getVerifMitra();
+    })
+  }
+
+  const deleteMitra = (id) => {
+    axios
+    .delete(`https://group4.altaproject.online/mitra/${id}`,
+    {
+      headers : { Authorization: `Bearer ${getCookie("Token")}`}
+    }
+    )
+
+    .then((res) => {
+      alert("akun berhasil dihapus")
+      getListMitra();
+    })
+  }
+
   useEffect(() => {
     getVerifMitra();
     getListMitra();
@@ -65,8 +113,8 @@ const adminverification = () => {
                 </button>
               </Col>
               <Col sm={6} className="p-0 btnright">
-                <button className="me-4 btnacc">Terima</button>
-                <button className="me-4 btnrej">Tolak</button>
+                <button className="me-4 btnacc"  onClick={() => accept(user.id)}>Terima</button>
+                <button className="me-4 btnrej"  onClick={() => denied(user.id)}>Tolak</button>
               </Col>
             </Row>
           );
@@ -83,7 +131,7 @@ const adminverification = () => {
                 </button>
               </Col>
               <Col sm={6} className="p-0 btnright">
-                <button className="me-4 btnrej">Hapus</button>
+                <button className="me-4 btnrej" onClick={() => deleteMitra(user.id)}>Hapus</button>
               </Col>
             </Row>
           );
